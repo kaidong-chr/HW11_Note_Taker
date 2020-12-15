e2e5cf04efd125f1b186324cba3d772d047b4f0d
Kai Dong <dongkai981@gmail.com>
2020-12-14 20:51:11 -0600

    Added comments to server.js

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+// Dependency required for our server to function
 const express = require("express");
-const fs = require("fs");
 
+// Setting up the express server
 const app = express();
 
+// Port for our server
 const PORT = process.env.PORT || 3001;
 
+// Express app data parsing 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Express app file usage in public directory 
 app.use(express.static("./public"));
 
-// require("./routes/apiRoutes")(app);
+// Route files that gives our server a map of how to respond when user visit or request data from various URL paths
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+// Starts our server to begin listening
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
