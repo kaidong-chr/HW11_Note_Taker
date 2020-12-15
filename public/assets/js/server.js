876250406f6bca1fc00ef0d894ede7ba4707ded4
Kai Dong <dongkai981@gmail.com>
2020-12-14 20:04:46 -0600

    Added routes folder with separate api and html routes js files

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,5 +1,4 @@
 let express = require("express");
-let path = require("path");
 let fs = require("fs");
 
 let app = express();
@@ -9,29 +8,8 @@ let PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
-});
-
-app.get("/api/notes"), function(req, res) {
-    let notes = req.body;
-
-}
-
-app.post("/api/notes"), function(req, res) {
-    let newNotes = req.body;
-
-    
-}
-
-app.delete("/api/notes:id"), function (req, res) {
-    
-}
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
---
+++ routes/apiRoutes.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+
+app.get("/api/notes"), function(req, res) {
+    return 
+
+}
+
+app.post("/api/notes"), function(req, res) {
+    let newNotes = req.body;
+
+    
+}
+
+app.delete("/api/notes:id"), function (req, res) {
+
+}
